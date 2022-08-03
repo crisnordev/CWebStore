10c749ff44b11ae54a2d9f368dd6cfa53c72e53c
CristianoBNoronha <cris_nor@hotmail.com>
2022-08-03 17:56:36 -0300

    Creating CWebStoreDbContext, CategoryMap, ProductMap.

---
+++ CWebStore.Infra/Data/CWebStoreDbContext.cs
@@ -0,0 +1,28 @@
+﻿using CWebStore.Domain.Entities;
+using CWebStore.Infra.Data.Mappings;
+using Microsoft.EntityFrameworkCore;
+
+namespace CWebStore.Infra.Data;
+
+public class CWebStoreDbContext : DbContext
+{
+    
+        public CWebStoreDbContext(DbContextOptions<CWebStoreDbContext> options) : base(options) { }
+
+        public DbSet<Category>? Categories { get; set; }
+
+        public DbSet<Product>? Products { get; set; }      
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            
+            options.UseSqlServer("Server=localhost,1433;Database=CwebStore;User ID=sa;Password=1q2w3e4r@#$");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+            modelBuilder.ApplyConfiguration(new ProductMap());
+            base.OnModelCreating(modelBuilder);
+        }
+}
---
+++ CWebStore.Infra/Data/Mappings/CategoryMap.cs
@@ -0,0 +1,29 @@
+﻿using CWebStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CWebStore.Infra.Data.Mappings;
+
+public class CategoryMap  : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.ToTable("Category");
+
+        builder.HasKey(x => x.Id);
+
+        builder.OwnsOne(x => x.CategoryName)
+            .Property(x => x.Name)
+            .HasColumnName("CategoryName")
+            .HasColumnType("NVARCHAR")
+            .HasMaxLength(80)
+            .IsRequired();
+            
+        builder.OwnsOne(x => x.CategoryName)
+            .Ignore(x => x.Notifications);
+            
+        builder.Ignore(x => x.Notifications);
+
+
+    }
+}
